2427d2ccfe550132c5405c5b5dc57edfcbc71be4
dimpal001 <dimpaldas518@gmail.com>
2024-07-13 02:11:06 +0530

    resolve prepare argument page

--- src/Pages/LiquidText/LiquidTextMainPage.jsx
+++ src/Pages/LiquidText/LiquidTextMainPage.jsx
@@ -11,10 +11,12 @@ import CreateLTModal from './CraeteLTModal'
 import ConfirmDeleteModal from './ConfirmDeleteModal'
 import ShareUserModal from './ShareUserModal'
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url
-).toString()
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.mjs',
+//   import.meta.url
+// ).toString()
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
 
 const options = {
   cMapUrl: '/cmaps/',
